dbda5f45e47c7e5e4cb581702e3b98dce0a246d4
SiamakSm <siamak036@gmail.com>
2026-02-27 18:34:03 +0100

    Use Render DATABASE_URL with SSL

--- src/db.js
+++ src/db.js
@@ -1,15 +1,39 @@
 // db.js
 // 
 
+
+// With Render
+
 require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
+
+
+
+
+
+// WithOut Render
+
+//require("dotenv").config();
+//const { Pool } = require("pg");
+//
+//const pool = new Pool({
+//  host: process.env.DB_HOST,
+//  port: process.env.DB_PORT,
+//  database: process.env.DB_NAME,
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASSWORD,
+//});
+//
+//module.exports = pool;
+
+
+
