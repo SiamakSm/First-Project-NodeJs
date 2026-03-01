22447e39bf032d8a7ffd00a02d35f2fdb43033da
SiamakSm <siamak036@gmail.com>
2026-03-01 20:12:56 +0100

    Adding local Server along side Render server

--- src/db.js
+++ src/db.js
@@ -6,12 +6,11 @@
 
 require("dotenv").config();
 const { Pool } = require("pg");
+const isProd = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: isProd ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
